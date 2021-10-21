b559ca44479a996e105d293abc78477756e9874e
Adelina <adelina.romanova05@yandex.ru>
2021-10-21 14:58:22 +0400

    Commit

---
+++ laba_1/laba_1/FormPlane.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba_1
+{
+    public partial class FormPlane : Form
+    {
+        private Plane plane;
+        public FormPlane()
+        {
+            InitializeComponent();
+        }
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxPlane.Width, pictureBoxPlane.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            plane.DrawTransport(gr);
+            pictureBoxPlane.Image = bmp;
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            plane = new Plane();
+            plane.Init(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Black, Color.FromArgb(64, 0, 0), true, true, true);
+            plane.SetPosition(rnd.Next(10, 400), rnd.Next(10, 400), pictureBoxPlane.Width, pictureBoxPlane.Height);
+            Draw();
+        }
+
+		private void buttonMove_Click(object sender, EventArgs e)
+		{
+			//получаем имя кнопки
+			string name = (sender as Button).Name;
+			switch (name)
+			{
+				case "buttonUp":
+					if (plane != null)
+					{
+						plane.MoveTransport(Direction.Up);
+					}
+					else
+					{
+						MessageBox.Show("Сначала создайте объект!");
+						return;
+					}
+					break;
+				case "buttonDown":
+					if (plane != null)
+					{
+						plane.MoveTransport(Direction.Down);
+					}
+					else
+					{
+						MessageBox.Show("Сначала создайте объект!");
+						return;
+					}
+					break;
+				case "buttonLeft":
+					if (plane != null)
+					{
+						plane.MoveTransport(Direction.Left);
+					}
+					else
+					{
+						MessageBox.Show("Сначала создайте объект!");
+						return;
+					}
+					break;
+				case "buttonRight":
+					if (plane != null)
+					{
+						plane.MoveTransport(Direction.Right);
+					}
+					else
+					{
+						MessageBox.Show("Сначала создайте объект!");
+						return;
+					}
+					break;
+			}
+			Draw();
+		}
+	}
+}
